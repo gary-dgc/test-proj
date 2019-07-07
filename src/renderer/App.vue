60fa192832e1842039bfaf585e02a15f55faa1f2
gary-dgc <gary.diao@yahoo.com>
2019-07-07 19:43:04 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+        <VueDropdown label="Menu">
+          <VueDropdownButton icon-left="edit">Edit</VueDropdownButton>
+          <VueDropdownButton icon-left="delete">Delete</VueDropdownButton>
+          <VueDropdownButton icon-left="lock" keep-open>Keep open</VueDropdownButton>
+        </VueDropdown>
+    <VueButton v-tooltip="`Detailed explanation here`">Hover me!</VueButton>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test-proj'
+  }
+</script>
+<style lang="stylus">
+  @import "~@vue/ui/dist/vue-ui.css"
+
+</style>
+<style>
+  /* CSS */
+</style>
